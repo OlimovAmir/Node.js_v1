6f61e606d7000bb99aae97996e047b6c7947bca7
olimov Amir <olimov.amir@mail.ru>
2023-10-20 10:23:47 +0300

    v2

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const yargs = require('yargs');
-
+const notes = require('./notes')
 yargs.command({
     command: 'add',
     describe: 'Добавить заметку',
-    handler(){
-        console.log('Add command')
+    builder: {
+        title: {
+            type: 'string',
+            demandOption: true,
+            describe: 'Name title',
+        },
+        text: {
+            type: 'string',
+            demandOption: true,
+            describe: 'Content',
+        },
+    },
+    handler({title, text}){
+        notes.addNote(title, text)
     }
 })
 yargs.parse();
---
+++ notes.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const notePath = path.join(__dirname, 'note.json'); // получаем доступ до файла
+
+const addNote = (title, text) => {
+    //console.log((`${title} `) + (` ${text}`));
+    getNote((note)=>{
+        console.log('addNote', note)
+        const dublicateNote = note.find(noteEtem =>noteEtem.title === title) // проверка на дубликат
+        if (dublicateNote) {
+            console.log('Заметка существует')
+        } else {
+            note.push({title, text})
+            console.log('Заметка добавлена')
+        }
+    })
+}
+
+const getNote = (callback) => {
+    fs.readFile(notePath, 'utf-8', (error, content)=>{
+        if (error) {
+            throw new Error(error)
+        }
+        try {
+            callback(JSON.parse(content)) 
+        } catch (error) {
+            callback([]) 
+        }
+        //console.log(content)
+    })
+}
+
+module.exports = {
+    addNote
+}
